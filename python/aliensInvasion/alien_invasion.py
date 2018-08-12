91482eb4b1a3ef32442dc282d2197d4c80664dee
lijinglin <lijinglin@qq.com>
2018-08-12 10:18:24 +0800

    add bullet running

--- python/aliensInvasion/alien_invasion.py
+++ python/aliensInvasion/alien_invasion.py
@@ -4,15 +4,17 @@ import pygame
 from settings import Settings
 from ship import Ship
 from game_manager import GameFunctions
-
+from pygame.sprite import Group
 def run_game():
 	pygame.init()
 	ai_setting = Settings()
 	screen = pygame.display.set_mode(ai_setting.screen)
 	aShip = Ship(screen);
-	gf = GameFunctions(ai_setting,screen,aShip)
+	bullets = Group()
+	gf = GameFunctions(ai_setting,screen,aShip,bullets)
 	pygame.display.set_caption('Alien Invasion')
 	bgcolor = ai_setting.bgcolor
+	
 	while True:
 		gf.check_events()
 		gf.update_screen();
--- python/aliensInvasion/game_manager.py
+++ python/aliensInvasion/game_manager.py
@@ -2,20 +2,32 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from pygame.sprite import Group
 
 def log(obj):
 	print(str(obj))
 	
 class GameFunctions():
-	def __init__(self,settings,screen,ship):
+	def __init__(self,settings,screen,ship,bullets):
 		self.settings = settings
 		self.screen = screen;
 		self.ship = ship;
+		self.bullets = bullets
 	
 	def update_screen(self):
 		self.screen.fill(self.settings.bgcolor)
+		for bullet in self.bullets.sprites():
+			if bullet.rect.bottom < 0:
+				del bullet
+				self.bullets.remove(bullet)
+			else:
+				bullet.update()
+				bullet.draw_bullet()
 		self.ship.blitme()
 		pygame.display.flip()
+		
+		
 
 	def key_events_handle(self,event):
 		if event.type == pygame.KEYDOWN or  event.type == pygame.KEYUP:
@@ -24,12 +36,16 @@ class GameFunctions():
 				
 			if(event.key == pygame.K_LEFT ):
 				self.ship.move_left = (event.type == pygame.KEYDOWN)
-			
-			if(event.key == pygame.K_UP ):
+			elif(event.key == pygame.K_UP ):
 				self.ship.move_up = (event.type == pygame.KEYDOWN);
 				
-			if(event.key == pygame.K_DOWN ):
+			elif(event.key == pygame.K_DOWN ):
 				self.ship.move_down = (event.type == pygame.KEYDOWN)
+				
+			elif(event.key == pygame.K_SPACE and event.type == pygame.KEYDOWN):
+				bullet = Bullet(self.settings,self.screen,self.ship)
+				self.bullets.add(bullet)
+			
 			
 	
 	def check_events(self):
